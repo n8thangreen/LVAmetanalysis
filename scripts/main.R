6a6d503e89d7bf188d91c9a27a6efc43df9e8176
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-05-15 18:09:16 +0100

    runs with real data set
    * results look about right
    * need to check and compare against previous, separate meta analysis

--- scripts/main.R
+++ scripts/main.R
@@ -7,66 +7,68 @@ library(rjags)
 library(R2jags)
 library(dplyr)
 
-dat <- haven::read_dta("data/LVA and outcomes.dta")
+dat_raw <- haven::read_dta("data/LVA and outcomes.dta")
 
-# fake data
+dat <-
+  dat_raw |> 
+  select(study, imaging, cohort, aneurysm) |> 
+  rename(N = cohort,
+         n = aneurysm) |> 
+  mutate(
+    # study = as.numeric(as.factor(study)),
+    imaging = ifelse(imaging == "CMR+TTE",
+                     "CMR", imaging))
 
-# hard-coded
-fdat <- data.frame(z = c(0,0,1,1,NA,NA,0,1),
-                   x = c(0,1,0,1,0,1,NA,NA),
-                   n = c(12,3,5,18,10,20,5,10),
-                   study = c(1,1,1,1,2,2,3,3))
+maron2008 <- 
+  tibble::tribble(
+    ~study, ~n,	~z,	~x,
+    "Maron 2008", 1271, 0, NA,	
+    "Maron 2008", 12,	1,	0,
+    "Maron 2008", 10,	1,	1,
+    "Maron 2008", 6,	NA,	1)
 
-offset <- c(0, cumsum(fdat$n)) + 1
+dat_agg <- 
+  rbind(
+    dat |> 
+      mutate(z = ifelse(imaging == "CMR", 1, NA),
+             x = ifelse(imaging == "TTE", 1, NA)),
+    dat |> 
+      mutate(n = N - n,
+             z = ifelse(imaging == "CMR", 0, NA),
+             x = ifelse(imaging == "TTE", 0, NA))) |> 
+  select(-imaging, -N) |> 
+  arrange(study) |> 
+  filter(study != "Maron 2008") |> 
+  rbind(maron2008) |> 
+  mutate(study_id = as.numeric(as.factor(study)))
+
+offset <- c(0, cumsum(dat_agg$n)) + 1
 
 x <- z <- NULL
-study <- NULL
+study_id <- NULL
 
 # create equivalent individual data
-for (j in 1:nrow(fdat)) {
+for (j in 1:nrow(dat_agg)) {
   for (i in offset[j]:(offset[j+1]-1)) {
-    x[i] <- fdat$x[j]
-    z[i] <- fdat$z[j]
-    study[i] <- fdat$study[j]
+    x[i] <- dat_agg$x[j]
+    z[i] <- dat_agg$z[j]
+    study_id[i] <- dat_agg$study_id[j]
   }  
 }
 
-dataJags <-
-  list(x = x,                       # imperfect test
-       z = z,                       # gold standard
-       study = study,               # study id
-       ns = length(unique(study)),  # number of studies
-       n = length(x))               # number of patients
-
-# from generative distribution
-prev <- 0.7
-indiv_dat <- 
-  data.frame(study = rep(1:3, times = c(10,10,10))) |> 
-  group_by(study) |> 
-  mutate(id = 1:n(),
-         sprev = boot::inv.logit(rnorm(1, car::logit(prev), 0.5))) |> 
-  ungroup() |> 
-  mutate(z = rbinom(n = n(), size = 1, prob = sprev),
-         sens = 0.9,
-         spec = 0.8) |> 
-  rowwise() |> 
-  mutate(x = ifelse(z == 1,
-                    rbinom(n = 1, size = 1, prob = sens),
-                    rbinom(n = 1, size = 1, prob = 1-spec)),
-         x = ifelse(study == 2, NA, x),
-         z = ifelse(study == 3, NA, z))
+prev <- 0.2
 
 # priors
-beta_pars_sens <- MoM_beta(0.9, 0.05)
+beta_pars_sens <- MoM_beta(0.8, 0.1)
 beta_pars_1m_spec <- MoM_beta(1 - 0.8, 0.1)
 mu_beta <- car::logit(prev)
 
 dataJags <-
-  list(x = indiv_dat$x,                       
-       z = indiv_dat$z,                       
-       study = indiv_dat$study,               
-       ns = length(unique(indiv_dat$study)),  
-       n = nrow(indiv_dat))
+  list(x = x,                       
+       z = z,                       
+       study = study_id,               
+       ns = length(unique(study_id)),  
+       n = length(x))
 
 filein <- "BUGS/bugs_script.txt"
 params <- c("phi", "psi", "psi0", "beta0")
---
+++ scripts/main_fake_data.R
@@ -0,0 +1,95 @@
+
+# prevalence analysis
+# imperfect test and gold-standard
+# using fake data
+# with BUGS
+
+library(rjags)
+library(R2jags)
+library(dplyr)
+
+# fake data
+
+# hard-coded
+fdat <- data.frame(z = c(0,0,1,1,NA,NA,0,1),
+                   x = c(0,1,0,1,0,1,NA,NA),
+                   n = c(12,3,5,18,10,20,5,10),
+                   study = c(1,1,1,1,2,2,3,3))
+
+offset <- c(0, cumsum(fdat$n)) + 1
+
+x <- z <- NULL
+study <- NULL
+
+# create equivalent individual data
+for (j in 1:nrow(fdat)) {
+  for (i in offset[j]:(offset[j+1]-1)) {
+    x[i] <- fdat$x[j]
+    z[i] <- fdat$z[j]
+    study[i] <- fdat$study[j]
+  }  
+}
+
+dataJags <-
+  list(x = x,                       # imperfect test
+       z = z,                       # gold standard
+       study = study,               # study id
+       ns = length(unique(study)),  # number of studies
+       n = length(x))               # number of patients
+
+# from generative distribution
+prev <- 0.7
+indiv_dat <- 
+  data.frame(study = rep(1:10, each = 10)) |> 
+  group_by(study) |> 
+  mutate(id = 1:n(),
+         sprev = boot::inv.logit(rnorm(1, car::logit(prev), 0.5))) |> 
+  ungroup() |> 
+  mutate(z = rbinom(n = n(), size = 1, prob = sprev),
+         sens = 0.9,
+         spec = 0.8) |> 
+  rowwise() |> 
+  mutate(x = ifelse(z == 1,
+                    rbinom(n = 1, size = 1, prob = sens),
+                    rbinom(n = 1, size = 1, prob = 1-spec)),
+         x = ifelse(study == 2, NA, x),
+         z = ifelse(study == 3, NA, z))
+
+# priors
+beta_pars_sens <- MoM_beta(0.9, 0.05)
+beta_pars_1m_spec <- MoM_beta(1 - 0.8, 0.1)
+mu_beta <- car::logit(prev)
+
+dataJags <-
+  list(x = indiv_dat$x,                       
+       z = indiv_dat$z,                       
+       study = indiv_dat$study,               
+       ns = length(unique(indiv_dat$study)),  
+       n = nrow(indiv_dat))
+
+filein <- "BUGS/bugs_script.txt"
+params <- c("phi", "psi", "psi0", "beta0")
+
+n.iter <- 20000
+n.burnin <- 1000
+n.thin <- floor((n.iter - n.burnin)/500)
+
+res_bugs <-
+  jags(data = dataJags,
+       inits = NULL,
+       parameters.to.save = params,
+       model.file = filein,
+       n.chains = 1,
+       n.iter,
+       n.burnin,
+       n.thin,
+       DIC = TRUE)
+
+# print(res_bugs)
+# mcmcplot(res_bugs)
+# plots <- traceplot(res_bugs)
+
+R2WinBUGS::attach.bugs(res_bugs$BUGSoutput)
+
+output <- res_bugs$BUGSoutput
+output
